1cec31c65fc815f6c3b82f791e119b4a1fa5c573
karmenfrbezar <karmen.frbezar7@gmail.com>
2026-01-12 10:48:27 +0100

    Update početna

--- trenerko/src/pages/IndexPage.vue
+++ trenerko/src/pages/IndexPage.vue
@@ -1,8 +1,15 @@
 <template>
-  <q-page class="flex flex-center">
-    
+  <q-page class="flex flex-center bg-primary color">
+    <h2>Dobrodošli!</h2>
   </q-page>
 </template>
+
+<style>
+  h2 {
+    color:white;
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
+</style>
 
 <script setup>
 //
