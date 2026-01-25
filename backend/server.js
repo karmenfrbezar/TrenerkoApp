2c28a9c82aa552486cb446aeb0df6562740ef501
imamarii123 <imamarii123@gmail.com>
2026-01-25 22:03:31 +0100

    server za produkciju

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.json());
